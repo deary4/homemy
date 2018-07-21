b3476bfa0bf7d4d036513e3d857b1e3bd645c2c0
deary4 <worachet.deary@gmail.com>
2018-07-21 23:39:10 +0700

    start

---
+++ src/main/java/com/md/homemy/HomemyApplication.java
@@ -0,0 +1,12 @@
+package com.md.homemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomemyApplication.class, args);
+    }
+}
---
+++ src/main/java/com/md/homemy/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.md.homemy.controller;
+
+import apple.laf.JRSUIConstants;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String index() {
+        return "Hello my home 169 Mod Dear";
+    }
+}
+
